624cca6b6f3789134e05f8bf0ed970b253d9dda3
novaari2 <novaari20@gmail.com>
2024-03-27 16:33:37 +0700

    migrasi komponen

--- src/App.js
+++ src/App.js
@@ -1,24 +1,23 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import './tailwind.css';
+import Header from './components/Header';
+import Hero from './components/Hero';
+import Browse from './components/Browse';
+import Arrive from './components/Arrive';
+import Clients from './components/Clients';
+import Footer from './components/Footer';
+import AsideMenu from './components/AsideMenu';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+    <Header />
+    <Hero />
+    <Browse />
+    <Arrive />
+    <Clients />
+    <AsideMenu />
+    <Footer />
     </div>
   );
 }
